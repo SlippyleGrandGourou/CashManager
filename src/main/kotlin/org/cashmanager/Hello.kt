4b3396986ae01764ec5065343bce88ab85568cd5
Bartosz <bartosz.truchlewski@epitech.eu>
2019-11-02 10:11:11 +0100

    Basics

---
+++ src/main/kotlin/org/cashmanager/Hello.kt
@@ -0,0 +1,6 @@
+package org.cashmanager
+
+fun main(args: Array<String>) {
+    println("Hello, World")
+}
+
---
+++ src/test/kotlin/org/cashmanager/HelloTest.kt
@@ -0,0 +1,8 @@
+package org.cashmanager
+
+import org.junit.Test
+import kotlin.test.assertEquals
+
+class HelloTest {
+
+}
